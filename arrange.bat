3911e1dc89a0022f9578e9a83811818528baf200
yogiksuryatmika19 <yogiksuryatmika@gmail.com>
2026-05-24 19:44:38 +0800

    change arrange.bat logic

--- arrange.bat
+++ arrange.bat
@@ -6,17 +6,37 @@ set "DOCS_DIR=docs"
 for /d %%D in ("%DOCS_DIR%\*") do (
     set "FOLDER_NAME=%%~nxD"
 
-    rem Skip stylesheets and javascripts
-    if /I not "!FOLDER_NAME!"=="stylesheets" if /I not "!FOLDER_NAME!"=="javascripts" (
+    rem Skip specific folders
+    if /I not "!FOLDER_NAME!"=="stylesheets" if /I not "!FOLDER_NAME!"=="javascripts" if /I not "!FOLDER_NAME!"=="Images" (
 
         rem Create/overwrite arrange.txt
         > "%%D\arrange.txt" (
+
+            rem Header
+            echo nav:
+            echo.
+
+            rem List all markdown files
             for %%F in ("%%D\*.md") do (
                 echo - %%~nxF
             )
+
+            rem Empty line before footer
+            echo.
+
+            rem Footer
+            echo - ...
         )
 
         echo Generated: %%D\arrange.txt
+
+        rem Check if .pages exists
+        if not exist "%%D\.pages" (
+            type nul > "%%D\.pages"
+            echo Created: %%D\.pages
+        ) else (
+            echo Skipped existing: %%D\.pages
+        )
     )
 )
 
